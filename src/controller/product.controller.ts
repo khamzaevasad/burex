784b602a37484d47f1633794b912f7765d52977e
khamzaevasad <xamzayevasad442@gmail.com>
2025-10-27 07:58:40 +0900

    Feat: modify updateChosenProduct logic

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -44,9 +44,12 @@ productController.createNewProduct = async (req: ReqAdmin, res: Response) => {
 };
 
 // updateChosenProduct
-productController.updateChosenProduct = (req: Request, res: Response) => {
+productController.updateChosenProduct = async (req: Request, res: Response) => {
   try {
     console.log("updateChosenProduct");
+    const id = req.params.id;
+    const result = await productService.updateChosenProduct(id, req.body);
+    res.status(HttpCode.OK).json({ data: result });
   } catch (err) {
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
--- src/model/Product.service.ts
+++ src/model/Product.service.ts
@@ -1,7 +1,8 @@
-import { ProductInput } from "../libs/types/product";
+import { ProductInput, ProductUpdateInput } from "../libs/types/product";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import ProductModel from "../schema/Product.model";
 import { Product } from "../libs/types/product";
+import { shapeIntoMongooseObjectId } from "../libs/config";
 
 class ProductService {
   private readonly productModel;
@@ -10,6 +11,9 @@ class ProductService {
     this.productModel = ProductModel;
   }
 
+  // SPA
+
+  // BSSR
   public createNewProduct = async (input: ProductInput): Promise<Product> => {
     try {
       return await this.productModel.create(input);
@@ -17,6 +21,19 @@ class ProductService {
       console.log("Error, model:createNewProduct", err);
       throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
     }
+  };
+
+  public updateChosenProduct = async (
+    id: string,
+    input: ProductUpdateInput
+  ): Promise<Product> => {
+    id = shapeIntoMongooseObjectId(id);
+    const result = this.productModel.findOneAndUpdate({ _id: id }, input, {
+      new: true,
+    });
+    if (!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
+
+    return result;
   };
 }
 
