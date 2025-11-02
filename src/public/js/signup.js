4d76e1e38480a3b5896c776652e3860bac9745d8
khamzaevasad <xamzayevasad442@gmail.com>
2025-11-02 19:01:17 +0900

    feat: enhance signup form with image upload validation and preview

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,5 +1,31 @@
 console.log("Signup frontend javascript file");
 
+$(function () {
+  const fileTarget = $(".file-box .upload-hidden");
+  let filename;
+
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      const fileType = uploadFile["type"];
+      const validImageType = ["image/jpeg", "image/jpeg", "image/png"];
+
+      if (!validImageType.includes(fileType)) {
+        alert("Please insert only jpeg, jpg and png");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success ");
+        }
+        filename = $(this)[0].files[0].name;
+      }
+      $(this).siblings(".upload-name").val(filename);
+    }
+  });
+});
+
 $(function () {});
 
 function validateSignupForm() {
@@ -20,6 +46,15 @@ function validateSignupForm() {
 
   if (memberPassword !== memberConfirmPassword) {
     alert("Password differs please, check!");
+    return false;
+  }
+
+  const memberImage = $(".member-image").get(0).files[0]
+    ? $(".member-image").get(0).files[0].name
+    : null;
+
+  if (!memberImage) {
+    alert("please insert restaurant image");
     return false;
   }
 }
