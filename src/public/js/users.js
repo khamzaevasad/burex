d725d2d16e723dff8f1f3dc94f52cbb44d6ffd99
khamzaevasad <xamzayevasad442@gmail.com>
2025-11-05 16:35:23 +0900

    feat: implement dynamic member status selection in user table

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,1 +1,21 @@
 console.log("Users frontend javascript file");
+$(function () {
+  $(".member-status").on("change", (e) => {
+    const id = e.target.id;
+    const memberStatus = $(`#${id}.member-status`).val();
+
+    axios
+      .post("/admin/user/edit", { _id: id, memberStatus: memberStatus })
+      .then((response) => {
+        const result = response.data;
+        if (result) {
+          console.log("updated successfully");
+          $(".member-status").blur();
+        } else alert("User Updated Failed");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("User Updated Failed");
+      });
+  });
+});
